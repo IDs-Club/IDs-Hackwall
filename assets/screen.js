a19e2adcd46455a8a63e22ceed475bb8de1600b3
pob986 <pob986@163.com>
2013-08-30 05:38:38 +0800

    #1 修改微博显示样式

--- assets/screen.js
+++ assets/screen.js
@@ -4,7 +4,7 @@
         settings = {
             prefix  :   '$ ',
             max     :   50
-        }, lines = [], sp = $('<span class="sp" />');
+        }, lines = [], bk = [], sp = $('<span class="sp" />');
 
     function rand(l, u) {
         return Math.floor((Math.random() * (u-l+1))+l);
@@ -18,7 +18,9 @@
     }
 
     function println(str) {
+        bk.push(str);
         lines.push(str);
+        if (bk.length > 50) {bk.shift();};
     }
 
     function getWeibo() {
@@ -38,7 +40,7 @@
     }, 500);
 
     function strWalk() {
-        var str, state = 0, el, i, j;
+        var str, state = 0, wd, rt, wt, el, i, j;
         
         var interval = setInterval(function () {
             switch (state) {
@@ -50,35 +52,44 @@
                             $('.str:first').remove();
                         }
 
-                        el = $('<div class="str" />').appendTo(document.body);
+                        el = $('<div class="str WB_feed_datail S_line2 clearfix" />').appendTo(document.body);
+                        $('<div class="WB_face"><img width="50" height="50" src="'+ str.image_url +'" alt="" title="'+ str.user +'" ></div>').appendTo(el);
+                        wd = $('<div class="WB_detail" />').appendTo(el);
+                        $('<div class="WB_info">'+ str.user +'</div>').appendTo(wd);
+                        wt = $('<div node-type="feed_list_content" class="WB_text" />').appendTo(wd);
                         state = 1;
                         i = 0;
-                        str.text = str.user + ' : ' + str.text;
-                        if (str.origin != '') {
-                            if (str.origin.type == 1) {
-                                str.text += ' （转）@' + str.origin.user + ':' + str.origin.text;
-                            }else{
-                                str.text += ' （评）//@' + str.origin.user + ':' + str.origin.text;
-                            }
-                        };
                     }
                     break;
                 case 1:
                     // print
-                    el.html('<span class="head">' + settings.prefix + '</span>' + str.text.substring(0, i)).append(sp);
-                    document.body.scrollTop = document.body.scrollHeight;
+                    wt.html(str.text.substring(0, i));
                     if (i >= str.text.length) {
                         if (str.thumb != '') {
-                            el.append('<br><img src="'+ str.thumb +'">');
+                            wd.append('<img src="'+ str.thumb +'">');
+                        }
+                        if (str.origin != ''){
+                            if (str.origin.type == 1){
+                                rt = '<div class="WB_media_expand SW_fun2 S_line1 S_bg1"><div class="WB_arrow"><em class="S_line1_c">◆</em><span class="S_bg1_c">◆</span></div>';
+                                rt += '<div><div class="WB_name"><span class="blue">@'+ str.origin.user +'</span></div><div node-type="feed_list_reason" class="WB_text"><em>'+ str.origin.text +'</em></div></div></div>';
+                                $(rt).appendTo(wd);
+                            }else if (str.origin.type == 2) {
+                                rt = '<div class="WB_media_expand SW_fun2 S_line1 S_bg1"><div class="WB_arrow"><em class="S_line1_c">◆</em><span class="S_bg1_c">◆</span></div>';
+                                rt += '<div><div node-type="feed_list_reason" class="WB_text"><em>评论 <span class="blue">@' + str.origin.user + '</span> 的微博 “' + str.origin.text.substr(0, 20) + '...“</em></div></div></div>';
+                                $(rt).appendTo(wd);
+                            }   
                         }
                         state = 2;
                         j = 0;
-                    };      
+                    }; 
+                    document.body.scrollTop = document.body.scrollHeight;
+                    wd.append(sp);
                     i ++;
                     break;
                 case 2:
-                    if (j >= 150) {
+                    if (j >= 300) {
                         state = 0;
+                        if (lines.length == 0) {lines = bk.slice(0)};
                     }
                     j ++;
                     break;
@@ -102,6 +113,6 @@
             }
 
             getWeibo();
-        }, 15000);
+        }, 60000);
     });
 })();

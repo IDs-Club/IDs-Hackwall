ebd128830a94a55a8f891fb550a1ac7408dfb9a3
pob986 <pob986@163.com>
2013-08-31 03:23:24 +0800

    定时更新微博用户信息，增加微博表情匹配

--- assets/screen.js
+++ assets/screen.js
@@ -6,6 +6,9 @@
             max     :   50
         }, lines = [], bk = [], sp = $('<span class="sp" />');
 
+    var base_url = 'http://event-idsclub.ap01.aws.af.cm/';
+    var emotions;
+
     function rand(l, u) {
         return Math.floor((Math.random() * (u-l+1))+l);
     }
@@ -24,7 +27,7 @@
     }
 
     function getWeibo() {
-        $.getJSON('http://event-idsclub.ap01.aws.af.cm/weibolist.php', function (comments) {
+        $.getJSON(base_url + 'weibolist.php', function (comments) {
             if (comments && comments.length > 0) {
                 lines.length = 0;
                 for (var i = 0; i < comments.length; i ++) {
@@ -32,6 +35,24 @@
 
                     println(c);
                 }
+            }
+            $('#loading').remove();
+        });
+    }
+
+    function getEmotions() {
+        $.getJSON(base_url + 'info.php?type=emotion', function (data) {
+            if (data.status == 'ok') {
+                emotions = data.emotions;
+            }
+        });
+    }
+
+    function getUser() {
+        $.getJSON(base_url + 'info.php?type=userinfo', function (data) {
+            if (data.status == 'ok') {
+                $('.say').html(data.user.screen_name);
+                $('.follow').html('粉丝（' + data.user.followers_count + '）');
             }
         });
     }
@@ -41,7 +62,7 @@
     }, 500);
 
     function strWalk() {
-        var str, state = 0, wd, rt, wt, el, i, j;
+        var str, state = 0, wd, rt, wt, el, i, j, emt;
         
         var interval = setInterval(function () {
             switch (state) {
@@ -66,25 +87,53 @@
                     // print
                     wt.html(str.text.substring(0, i));
                     if (i >= str.text.length) {
+                        // 替换表情
+                        wt.html(str.text.replace(/(\[[^\[\]]*\])/g,
+                            function($1){
+                                var temp = '';
+                                $.each(emotions,function(i,it){
+                                    if($1 == i){
+                                         temp = '<img src="' + it + '" title="' + i + '"/>';
+                                         return false;
+                                    }else{
+                                        temp = $1;
+                                    }
+                                });
+                                return temp;
+                            })
+                        );
                         if (str.thumb != '') {
-                            wd.append('<img src="'+ str.thumb +'">');
+                            wd.append('<img class="thumb" src="'+ str.thumb +'">');
                         }
                         if (str.origin != ''){
+                            emt = str.origin.text.replace(/(\[[^\[\]]*\])/g,
+                            function($1){
+                                var temp = '';
+                                $.each(emotions,function(i,it){
+                                    if($1 == i){
+                                         temp = '<img src="' + it + '" title="' + i + '"/>';
+                                         return false;
+                                    }else{
+                                        temp = $1;
+                                    }
+                                });
+                                return temp;
+                            });
                             if (str.origin.type == 1){
                                 rt = '<div class="WB_media_expand SW_fun2 S_line1 S_bg1"><div class="WB_arrow"><em class="S_line1_c">◆</em><span class="S_bg1_c">◆</span></div>';
-                                rt += '<div><div class="WB_name"><span class="blue">@'+ str.origin.user +'</span></div><div node-type="feed_list_reason" class="WB_text"><em>'+ str.origin.text +'</em></div></div></div>';
+                                rt += '<div><div class="WB_name"><span class="blue">@'+ str.origin.user +'</span></div><div node-type="feed_list_reason" class="WB_text"><em>'+ emt +'</em></div></div></div>';
                                 $(rt).appendTo(wd);
                             }else if (str.origin.type == 2) {
                                 rt = '<div class="WB_media_expand SW_fun2 S_line1 S_bg1"><div class="WB_arrow"><em class="S_line1_c">◆</em><span class="S_bg1_c">◆</span></div>';
-                                rt += '<div><div node-type="feed_list_reason" class="WB_text"><em>评论 <span class="blue">@' + str.origin.user + '</span> 的微博 “' + str.origin.text.substr(0, 20) + '...“</em></div></div></div>';
+                                rt += '<div><div node-type="feed_list_reason" class="WB_text"><em>评论 <span class="blue">@' + str.origin.user + '</span> 的微博 “' + emt.substr(0, 20) + '...“</em></div></div></div>';
                                 $(rt).appendTo(wd);
                             }   
                         }
                         state = 2;
                         j = 0;
                     }; 
-                    document.body.scrollTop = document.body.scrollHeight;
                     wd.append(sp);
+                    document.body.scrollTop = document.body.scrollHeight;
                     i ++;
                     break;
                 case 2:
@@ -103,11 +152,14 @@
     }
 
     $(document).ready(function () {
+        $('<div id="loading"><div></div></div>').appendTo(document.body);
+        getEmotions();
         resizeBody();
         strWalk();
         $(window).resize(resizeBody);
         getWeibo();
 
+        setInterval(function(){getUser();}, 300000);
         setInterval(function () {
             if (lines.length > 50) {
                 return;

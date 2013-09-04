57e352d440145a2995872dd7258965d4846ec088
pob986 <pob986@163.com>
2013-09-04 15:35:48 +0800

    微博用户access_token过期后重新授权

--- api/index.php
+++ api/index.php
@@ -22,17 +22,20 @@ if (mysqli_num_rows($result))
     $c = new SaeTClientV2( WB_AKEY , WB_SKEY , $row_set[0]['access_token'] );
     $weibo_uid = $row_set[0]['userid'];
     $user_message = $c->show_user_by_id($weibo_uid);//根据ID获取用户等基本信息
-	$user = array(
-		'id' => $user_message['id'], 
-		'screen_name' => $user_message['screen_name'], 
-		'followers_count' => $user_message['followers_count']
-		);
+    if (isset($user_message['id'])) 
+    {
+    	$user = array(
+			'id' => $user_message['id'], 
+			'screen_name' => $user_message['screen_name'], 
+			'followers_count' => $user_message['followers_count']
+			);
 
-	// 重设微博更新时间
-	$oDB->update('weibo_update', "weibo_uid = $weibo_uid", "last_comment = 0, last_statuse = 0, last_comm_me = 0, last_weibo = 0 ");
+		// 重设微博更新时间
+		$oDB->update('weibo_update', "weibo_uid = $weibo_uid", "last_comment = 0, last_statuse = 0, last_comm_me = 0, last_weibo = 0 ");
 
-	echo json_encode(array('status' => 'ok', 'user' => $user));
-	exit();
+		echo json_encode(array('status' => 'ok', 'user' => $user));
+		exit();
+    }	
 }
 
 $o = new SaeTOAuthV2( WB_AKEY , WB_SKEY );
